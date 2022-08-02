e5f2ec2f807fd3a593b4df414d9754ae49b80d03
ardiman-xyz <pc.ardiman98@gmail.com>
2022-08-03 03:56:33 +0700

    add routes

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Ardiman\BelajarPhpMvc\App\Router;
+use Ardiman\BelajarPhpMvc\Controller\HomeController;
+
+
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+
+Router::run();
