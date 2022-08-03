416a422c1c09d83a71743b3785441697d08eda59
ardiman-xyz <pc.ardiman98@gmail.com>
2022-08-04 02:46:16 +0700

    add app folder

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ardiman\BelajarPhpMvc\Controller;
+
+class HomeController
+{
+
+    public function index(): void
+    {
+        echo "HomeController.index()";
+    }
+
+    public function hello(): void
+    {
+        echo "HomeController.hello()";
+    }
+}
