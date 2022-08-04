2f178c49b4232d4a2f8ee9be0cb9641a24b7844d
ardiman-xyz <pc.ardiman98@gmail.com>
2022-08-05 04:17:20 +0700

    add config connection to database

---
+++ app/Config/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ardiman\BelajarPhpMvc\Config;
+
+use PDO;
+
+class Database
+{
+
+    private static ?PDO $pdo = null;
+
+    public static function getConnection(string $env = "test"): PDO
+    {
+        if (self::$pdo === null) {
+            // create new pdo
+            require_once __DIR__ . "/../../config/Database.php";
+
+            $config = getDatabaseConfig();
+            self::$pdo = new \PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password']
+            );
+        }
+
+        return self::$pdo;
+    }
+}
---
+++ tests/Config/DatabaseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ardiman\BelajarPhpMvc\Config;
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertSame;
+
+class DatabaseTest extends TestCase
+{
+
+    public function testGetConnection()
+    {
+        $connection = Database::getConnection();
+
+        assertNotNull($connection);
+    }
+
+    public function testGetConnectionSingleTon()
+    {
+        $connection1 = Database::getConnection();
+        $connection2 = Database::getConnection();
+
+        assertSame($connection1, $connection2);
+    }
+}
